bb18623fbeeed0b5a0f253dcc39d396fdabc5665
Aleksander Długopolski <aleks@dotpro.org>
2018-12-15 01:55:10 +0100

    Add basic Relocator entry point file

---
+++ lib/relocator.rb
@@ -0,0 +1,14 @@
+require "thor"
+
+require "relocator/version"
+require "relocator/refinements"
+require "relocator/support"
+require "relocator/support/underscore"
+require "relocator/path"
+require "relocator/path/formatter"
+require "relocator/cli"
+require "relocator/cli/success"
+require "relocator/cli/move"
+
+module Relocator
+end
