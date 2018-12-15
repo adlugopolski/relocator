da84a0906c6d776d726ed1dd775a77ca37947a43
Aleksander Długopolski <aleks@dotpro.org>
2018-12-15 01:55:35 +0100

    Remove default template spec from Relocator

--- spec/relocator_spec.rb
+++ spec/relocator_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Relocator do
   it "has a version number" do
     expect(Relocator::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
