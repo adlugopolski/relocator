483351d1e78e2b82d91f178f63a640de8f70bc99
Aleksander Długopolski <aleks@dotpro.org>
2018-12-16 13:10:44 +0100

    Remove file search logic from Move object

--- lib/relocator/cli/move.rb
+++ lib/relocator/cli/move.rb
@@ -5,33 +5,49 @@ class Relocator::CLI::Move
     new(*args).call
   end
 
-  def initialize(src, dst)
-    self.src = src
-    self.dst = dst
+  def initialize(source_class_name, dest_class_name)
+    self.source_class_name = source_class_name
+    self.dest_class_name = dest_class_name
   end
 
   def call
     return unless Relocator::CLI::Success.call(files)
+
+    STDOUT.puts "mv %s %s".format(code_source, code_dest)
+    STDOUT.puts "mv %s %s".format(spec_source, spec_dest)
+    STDOUT.puts replace_command
   end
 
   private
 
-  attr_accessor :src, :dst
+  attr_accessor :source_class_name, :dest_class_name
 
-  def class_name_regexp
-    "class %<name>s"
-      .format(name: src)
-      .yield_self { |string| Regexp.escape(string) }
-      .yield_self { |regexp| Regexp.new(regexp) }
+  def replace_command
+    "gsed -i 's/%s/%s/g' %s %s".format(
+      source_class_name,
+      dest_class_name,
+      code_dest,
+      spec_dest,
+    )
   end
 
   def files
-    @_files ||=
-      Dir["**/*.rb"].reject do |filename|
-        File
-          .open(filename)
-          .grep(class_name_regexp)
-          .empty?
-      end
+    @_files ||= Relocator::CLI::Files.call(source_class_name)
+  end
+
+  def code_source
+    files.first
+  end
+
+  def spec_source
+    Relocator::Path::Spec.call(code_source)
+  end
+
+  def code_dest
+    @_code_dest ||= Relocator::Path::Formatter.call(dest_class_name, code_source)
+  end
+
+  def spec_dest
+    @_spec_dest ||= Relocator::Path::Spec.call(code_dest)
   end
 end
--- spec/relocator/move_spec.rb
+++ spec/relocator/move_spec.rb
@@ -3,38 +3,52 @@ RSpec.describe Relocator::CLI::Move do
     let(:service) { described_class.new(src, dst) }
     let(:dst) { "source" }
     let(:src) { "destination" }
-    let(:files) { [] }
+    let(:files) { %w[filename] }
     let(:success) { false }
 
     before do
-      allow(Relocator::CLI::Success).to receive_messages(call: success)
-      allow(Dir).to receive_messages("[]": files)
+      allow(Relocator::CLI::Success)
+        .to receive_messages(call: success)
+      allow(Relocator::CLI::Files)
+        .to receive_messages(call: files)
+      allow(Relocator::Path::Formatter)
+        .to receive_messages(call: "dest")
+      allow(Relocator::Path::Spec)
+        .to receive_messages(call: "spec")
+      allow(STDOUT).to receive(:puts)
+
+      service.call
     end
 
-    context "without any files" do
-      it "returns nothing" do
-        expect(service.call).to be_nil
+    context "with no success" do
+      let(:success) { false }
+
+      it "does not output commands" do
+        expect(STDOUT).not_to have_received(:puts)
       end
     end
 
-    context "with one present file" do
-      let(:files) { %w[spec/support/data/some_file.rb] }
+    context "with data found" do
+      let(:success) { true }
 
-      xit "does peform rename" do
-        service.call
-      end
-    end
-
-    context "with two present files" do
-      let(:files) do
-        %w[
-          spec/support/data/some_file.rb
-          spec/support/data/other_file.rb
-        ]
+      it "outputs commands" do
+        expect(STDOUT)
+          .to have_received(:puts)
+          .exactly(3)
+          .times
       end
 
-      xit "returns nothing" do
-        expect(service.call).to be_nil
+      it "passed files to success object" do
+        expect(Relocator::CLI::Success)
+          .to have_received(:call)
+          .with(files)
+      end
+
+      it "uses spec to determine spec names" do
+        expect(Relocator::Path::Spec)
+          .to have_received(:call)
+          .with("filename")
+          .with("dest")
       end
     end
   end
